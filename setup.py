0868ec9fbfa15544fe3eebc92d59b36dcd085349
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2018-09-23 14:56:32 +0200

    Remove hyphen from package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sphinx-verboser',
+    name='sphinx_verboser',
     version='0.0.1',
     license='BSD',
     author='Bogdan Vukobratovic',
